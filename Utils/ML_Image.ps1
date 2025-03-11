f0f446ad19c2d15c7940f3c4812b3ad7a2335062
Michael lau <mikielau69@googlemail.com>
2025-03-11 17:30:52 +0000

    update Ml_build

--- Utils/ML_Image.ps1
+++ Utils/ML_Image.ps1
@@ -1,5 +1,6 @@
 $bootdrive = get-disk | Where-Object {$_.Number -eq '0'} | Select-Object -ExpandProperty Number
 
+
 $partition = Get-Partition -DiskNumber $bootdrive | Where-Object {$_.Size -ge '150GB'} | Select-Object -ExpandProperty PartitionNumber
 
 
@@ -9,6 +10,19 @@ Dism /Mount-Image /ImageFile:"C:\WinPE_amd64\media\sources\boot.wim" /Index:1 /M
 
 Dism /Mount-Image /ImageFile:"E:\boot.wim" /Index:1 /MountDir:"C:\mount"
 
-DiskPart /s x:\windows\system32\CreatePartitions-uefi.txt
-dism /apply-image /imagefile:l:\win-11-0125.wim /index:1 /applydir:c:
+
+
+
+$Partitions = Get-Partition -DiskNumber 0
+$Partitions | ForEach-Object { Get-Volume -Partition $_ }
+
+Initialize-Disk -Number 0 -PartitionStyle GPT
+
+get-disk -Number 0 | Get-Partition
+
+
+New-Partition -DiskNumber 0 -Size 512MB -GptType "{EBD0A0A2-B9E5-4433-87C0-68B6B72699C7}" | Format-Volume -FileSystem FAT32 -NewFileSystemLabel "EFI" -Confirm:$false
+
+
+
 
