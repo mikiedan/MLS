87654c3b006254a752f13522949d94bc789d4705
mikiedan <152700414+mikiedan@users.noreply.github.com>
2025-03-12 19:54:26 +0000

    updated WinPE process

--- Utils/ML_Build.ps1
+++ Utils/ML_Build.ps1
@@ -1,9 +1,11 @@
+#Mount the WinPE boot.wim file ready to inject required powershell cmdlets
+Mount-WindowsImage -ImagePath 'C:\bootwinpe\media\sources\boot.wim' -Index:1 -Path 'C:\bootwinpe\mount'
 
-#Add location of additional component as a variable
+#Add location of additional component as a variable 
 $WinADK="C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs"
 
 #Mount wim file into a temp folder prior to adding any additional packages or drivers to the image
-$WinPETemp='C:\winpe4'
+$WinPETemp='C:\bootwinpe\mount'
 
 #Load the required cab files into an array
 $CABfiles = @("$WinADK\en-gb\WinPE-WMI_en-gb.cab","$WinADK\en-gb\WinPE-NetFX_en-gb.cab", "$WinADK\en-gb\WinPE-Scripting_en-gb.cab","$WinADK\en-gb\WinPE-PowerShell_en-gb.cab","$WinADK\en-gb\WinPE-StorageWMI_en-gb.cab", "$WinADK\en-gb\WinPE-DismCmdlets_en-gb.cab")
@@ -13,8 +15,8 @@ Foreach ($CABfile in $CABfiles) {
   Add-WindowsPackage -PackagePath $CABFile -Path $WinPETemp 
 }
 
-#Once all cab or driver files loaded save and dismount the image from the temp location.
-Dismount-WindowsImage -path $WinPETemp -save
+#Once all cab or driver files loaded Discard or Save updated wim file and dismount the image from the temp location.
+Dismount-WindowsImage -path $WinPETemp -Save
 
 
 
--- Utils/ML_Create_WinPE.ps1
+++ Utils/ML_Create_WinPE.ps1
@@ -5,6 +5,12 @@ copype amd64 C:\WinPE_MLFiles
 #Run the following to mount the image to add files,drivers etc
 Dism /Mount-Image /ImageFile:C:\WinPE_MLFiles\media\sources\boot.wim /Index:1 /MountDir:C:\WinPE_MLFiles\mount
 
+
+
+
+
+
+
 #Once changes have been made dismount and Commit or Discard
 Dism /Unmount-Image /MountDir:C:\WinPE_MLFiles\mount /Discard
 
